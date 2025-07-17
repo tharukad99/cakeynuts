dc43602fb909df83536a5f63bfbbf22158927134
tharuka <trdilshan99@gmail.com>
2025-07-17 15:22:05 +0100

    web 1 st

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/slides/ba2.png' },
+              //{ src: 'images/happy-waitress-giving-coffee-customers-while-serving-them-coffee-shop.jpg' },
+              { src: 'images/ba3.png' },
+              { src: 'images/ba1.png' }
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
